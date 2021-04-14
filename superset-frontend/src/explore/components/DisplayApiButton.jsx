338a02583dcfa0381a8111e736ea02d91930b55e
drikc <cgampert@gmail.com>
2021-04-14 15:38:25 +0200

    feat: add a display /chart/data api button (apache/superset#11576): adapt to current HEAD

--- superset-frontend/src/explore/components/DisplayApiButton.jsx
+++ superset-frontend/src/explore/components/DisplayApiButton.jsx
@@ -18,7 +18,7 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Popover from 'src/common/components/Popover';
+import Popover from 'src/components/Popover';
 import { OverlayTrigger } from 'react-bootstrap';
 import { t, buildQueryContext } from '@superset-ui/core';
 
